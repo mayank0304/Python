e2b430078662a4247217e90348341cd8e92e6bbf
mayank0304 <mayank771768@gmail.com>
2024-03-17 08:17:12 +0530

    First Program

---
+++ Data types and Operatos/Tax1.py
@@ -0,0 +1,7 @@
+print("Mayank - 2203855")
+salary=int(input("Please enter your salary:"))
+tax = float(input("Please enter tax in percentage:"))
+totalTax = salary * tax /100
+remainingSalary = salary - totalTax
+print("Tax you have to pay on your salary:", round(totalTax ,2) )
+print("Your remaining salary after tax:", round(remainingSalary,2) )
