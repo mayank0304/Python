eb8b0af930711b5f8e07c2410a9521f91efa83ff
mayank0304 <mayank771768@gmail.com>
2024-03-17 08:23:14 +0530

    Python Programs till 4

---
+++ Data types and Operatos/SAcube2.py
@@ -0,0 +1,4 @@
+print("Mayank - 2203855")
+side = int(input("Enter edge of cube:"))
+surfaceArea = 6*(side**2) 
+print("Surface area of cube is:", surfaceArea)
---
+++ Data types and Operatos/Sphere4.py
@@ -0,0 +1,7 @@
+print("Mayank - 2203855")
+radius = float(input("Enter the radius of sphere:"))
+print("The radius of sphere is:",radius)
+print("The diameter of sphere is:", 2*radius)
+print("The circumference of sphere is:", 2*3.14*radius)
+print("The surfaceArea of sphere is:", 4*3.14*(radius**2))
+print("The volume of sphere is:", 4/3*(3.14)*(radius**3))
